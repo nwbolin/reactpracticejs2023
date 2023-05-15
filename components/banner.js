f372b27bbfa3521e3dccf25b945dbb51b5777c10
nwbolin <89266359+nwbolin@users.noreply.github.com>
2023-05-15 18:50:41 -0500

    Created Practice app from Node.js using Create cmd

---
+++ components/banner.js
@@ -0,0 +1,10 @@
+const Banner = () => {}
+    return (
+        <header>
+            <div>
+                Providing Houses all of the world.
+            </div>
+        </header>
+    )
+    
+export default Banner;
